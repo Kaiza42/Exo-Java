9b3c15e34267da70158916fc0d7eaab85efdfb68
Emerick <decdu59@live.fr>
2025-05-02 20:22:13 +0200

    Docs(main.java): add multiple exercice

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // Exo 1 Afficher le texte Hello World dans la console de sortie
+        // System.out.printf("Hello, World !");
+
+        // Exo 2 Afficher Reprendre l’exercice n°1 en utilisant une variable
+        // String Hello = "Hello, World !";
+        // System.out.println(Hello);
+
+        // Exo 3 perimetre d'un cercle
+//        double rayon = 5.0;
+//        double cercleRayon = 2 * Math.PI * rayon;
+//        System.out.println( "le perimetre d'un cercle est : " + cercleRayon);
+//
+//        // Exo 4 Afficher le périmètre d’un cercle dont le rayon est une variable
+//        Scanner inputAge = new Scanner(System.in);
+//
+//        System.out.print("Veuillez saisir votre age : ");
+//        int age = inputAge.nextInt();
+//        if(age < 18){
+//            System.out.println("Tu es mineur");
+//        }else{
+//            System.out.println("Tu es majeur");
+//        }
+        // Exo 5 Afficher le périmètre d’un cercle dont le rayon est demandé à l’utilisateur
+//        Scanner rayon = new Scanner(System.in);
+//        System.out.println("Entrer un chiffre : ");
+//        double saisieRayon = rayon.nextDouble();
+//        double cercleRayon = 2 * Math.PI * saisieRayon;
+//        System.out.println( "le perimetre d'un cercle est : " + cercleRayon);
+
+        // Exo 4 Afficher le périmètre d’un cercle dont le rayon est une variable
+//        Scanner inputAge = new Scanner(System.in);
+//
+//        System.out.print("Veuillez saisir votre age : ");
+//        int age = inputAge.nextInt();
+//        if(age < 18){
+//            System.out.println("Tu es mineur");
+//        }else{
+//            System.out.println("Tu es majeur");
+//        }
+
+      //  Exo 6 A partir d’une valeur saisie, vérifier si l’utilisateur est majeur ou mineur
+//        Scanner paire = new Scanner(System.in);
+//
+//        System.out.print("Veuillez saisir un chiffre : ");
+//
+//        int chiffre = paire.nextInt();
+//        if(chiffre % 2 == 0){
+//System.out.println("Le chiffre est paire");
+//        }else {
+//            System.out.println("Le chiffre est impaire");
+//        }
+
+        //Exo 7
+
+
+    }
+}
