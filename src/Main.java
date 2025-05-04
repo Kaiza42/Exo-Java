285d966ebbbede55b629f4d90ad0952b998e3d08
Emerick <decdu59@live.fr>
2025-05-04 15:26:43 +0200

    update(Exo.Java): add following exo in Java

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,5 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (int i = 1; i <= 255; i++) {
-            System.out.println("Code : " + i + " => Caractère : " + (char)i);
-        }
-
     }
 }
